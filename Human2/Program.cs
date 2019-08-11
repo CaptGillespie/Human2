5d5fbb839ef9f584d6556b40cf2a4d7c3856a827
Sean Gillespie <sean.gillespie22@gmail.com>
2019-08-11 11:57:30 -0700

    new commit

---
+++ Human2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Human2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+        public int Attack(Human target)
+        {
+            
+        }
+    }
+}
